5130b63aed1b85075aee1fd074460f5fb907f50f
sheikhsohag <sheikhsohag000@gmail.com>
2025-10-02 09:29:42 +0600

    bash scripts set up and print

---
+++ bash_scripts/print.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Hello World"
+
+
+
+
+
+
+# 🔹 Explanation
+
+# #!/bin/bash
+
+# Called a shebang.
+
+# Tells the system to use Bash shell to execute this script.
+
+# Must be the first line in the file.
+
+# echo "Hello World"
+
+# Prints Hello World to the terminal.
+
+# echo is a built-in Bash command for outputting text.
